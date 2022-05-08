f77ac78f6538185bc34f1175534bc2dd73b105db
Mayam0524 <maylina0524@gmail.com>
2022-05-09 00:47:04 +0900

    fix detail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let now = new Date();
-let days = ["Sunday", "Monaday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let month = months[now.getMonth()];

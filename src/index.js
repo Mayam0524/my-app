46c77f2feeb72cffaf1ac04cfde73bcad70810d4
Mayam0524 <maylina0524@gmail.com>
2022-05-08 23:26:36 +0900

    get daily forecast

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ if (minutes < 10) {
 }
 time.innerHTML = `${hour}:${minutes}`;
 
+//Daily Forecast
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -26,6 +27,41 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML = forecastHTML +
+            `<div class="col-2">
+                <div class="weather-forecast-date">
+                    ${formatDay(forecastDay.dt)}
+                </div>
+                <span id="next-day">
+                    <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="38">
+                </span>
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">
+                        ${Math.round(forecastDay.temp.max)}°
+                    </span>|
+                    <span class="weather-forecast-temperature-min">
+                        ${Math.round(forecastDay.temp.min)}°
+                    </span>
+                </div>
+            </div>`;
+        }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates){
+    let apiKey = "bde4594240bbd5634acfce653024105f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayForecast);
+}
 
 //Temperature and more
 function showTemp(response) {
@@ -53,39 +89,8 @@ function showTemp(response) {
     let wind = document.querySelector("#wind");
     let windElement = 
     wind.innerHTML = Math.round(response.data.wind.speed);
-}
 
-function displayForecast(response) {
-    let forecast = response.data.daily;
-
-    let forecastElement = document.querySelector("#forecast");
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {
-            forecastHTML = forecastHTML +
-            `<div class="col-2">
-                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-                <img
-                src="http://openweathermap.org/img/wn/${
-                    forecastDay.weather[0].icon
-                }@2x.png"
-                alt=""
-                width="38"
-                class="icon"
-                />
-                <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max"> ${Math.round(
-                    forecastDay.temp.max
-                )}° </span>
-                <span class="weather-forecast-temperature-min"> ${Math.round(
-                    forecastDay.temp.min
-                )}° </span>
-                </div>
-            </div>`;
-        }
-    });
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
+    getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -125,3 +130,6 @@ function showPosition(event) {
 
 let currentLocation = document.querySelector("#current");
 currentLocation.addEventListener("click", showPosition);
+
+//Beginning search
+searchCity("Tokyo");
